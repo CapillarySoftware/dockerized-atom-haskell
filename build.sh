25d3976951418e5540e0fa962d747fb9fb92a7b3
Isaac Shapira <fresheyeball@gmail.com>
2016-05-06 16:12:57 +0000

    specify fpco build version

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker run -v $(pwd):/work fpco/stack-build cp /usr/bin/stack /work/stack \
+docker run -v $(pwd):/work fpco/stack-build:lts-5.15 cp /usr/bin/stack /work/stack \
 && docker build -t localhost:5000/dev-haskell . \
 && sudo rm stack
